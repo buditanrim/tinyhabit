3a5c3634fc8710146473f8ebb65fec8af9f1fc9d
Buditanrim <1830184+buditanrim@users.noreply.github.com>
2023-09-24 13:51:28 +0700

    style: make the row of one habit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,12 +7,22 @@ let isActive = ref(true)
 
 <template>
   <main>
-    <div 
-      class="h-5 w-5 block bg-sky-500 border" 
-      :class="{ 'active' : isActive }"
-      @click="isActive = !isActive" 
-    >
-      a
+    <div class="flex">
+      <div class="m-2">
+        <p>Workout</p>
+      </div>
+      <div class="flex">
+        <div class="h-5 w-5 m-2 mr-1 bg-gray-200 border-gray-400" :class="{ 'active' : isActive }" @click="isActive = !isActive">
+        </div>
+        <div class="h-5 w-5 m-2 mr-1 bg-gray-200 border-gray-400" :class="{ 'active' : isActive }" @click="isActive = !isActive">
+        </div>
+        <div class="h-5 w-5 m-2 mr-1 bg-gray-200 border-gray-400" :class="{ 'active' : isActive }" @click="isActive = !isActive">
+        </div>
+        <div class="h-5 w-5 m-2 mr-1 bg-gray-200 border-gray-400" :class="{ 'active' : isActive }" @click="isActive = !isActive">
+        </div>
+        <div class="h-5 w-5 m-2 mr-1 bg-gray-200 border-gray-400" :class="{ 'active' : isActive }" @click="isActive = !isActive">
+        </div>
+      </div>
     </div>
   </main>
 </template>
