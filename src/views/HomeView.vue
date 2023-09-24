0145129480a2376244275c5aae0bfeca6332fdad
Buditanrim <1830184+buditanrim@users.noreply.github.com>
2023-09-24 13:41:33 +0700

    add greenbox

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,26 @@
 <script setup>
+import { ref } from 'vue'
+
+let isActive = ref(true)
+
 </script>
 
 <template>
   <main>
     <div 
-      class="h-5 w-5 bg-sky-500" 
+      class="h-5 w-5 block bg-sky-500 border" 
       :class="{ 'active' : isActive }"
-      @click="isActive = !isActive" ></div>
+      @click="isActive = !isActive" 
+    >
+      a
+    </div>
   </main>
 </template>
+
+<style>
+
+.active {
+background: green;
+}
+
+</style>
